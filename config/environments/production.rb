5e22fe0a540ec2bbd842e5f23288494e2bc0be91
Walker Cole <besidethebrave@gmail.com>
2017-02-08 16:32:36 -0500

    fixed variable settings for aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,10 +7,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('venmue'),
-      access_key_id: ENV.fetch('AKIAID5JRQCXZ7QX4ZLQ'),
-      secret_access_key: ENV.fetch('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'),
-      s3_region: ENV.fetch('US Standard'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
   # Eager load code on boot. This eager loads most of Rails and
