88493bc036e213d5782f34ce5d00a9c8199f9b75
Walker Cole <besidethebrave@gmail.com>
2017-02-09 11:15:29 -0500

    changed aws version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,13 +10,13 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('venmue'),
-      access_key_id: ENV.fetch('AKIAID5JRQCXZ7QX4ZLQ'),
-      secret_access_key: ENV.fetch('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'),
-      s3_region: ENV.fetch('US Standard'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
   
