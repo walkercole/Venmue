ef6a14c830d53a0deb9587fcbd04e325d78ad8d4
Walker Cole <besidethebrave@gmail.com>
2017-02-10 14:49:22 -0500

    added overlay banner to home header with transition button

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,23 +1,24 @@
-menu = {};
-menu.ready = function() {
-  var
-    $menuItem = $('.menu a.item, .menu .link.item'),
-    handler = {
-      activate: function() {
-        $(this)
-        .addClass('active')
-        .closest('.ui.menu')
-        .find('.item')
-        .not($(this))
-        .removeClass('active');
-      }
-    }
-  ;
-  $menuItem
-    .on('click', handler.activate)
-  ;
-};
-$(document).ready(menu.ready);
 $(document).ready(function(){
-    $(".rating").rating();
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
