c1a66c3aae03e6a955cdf1010aea0d22229a886b
Walker Cole <besidethebrave@gmail.com>
2017-02-11 15:55:07 -0500

    re styled the homepage and added 2 new features to the venue show page

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -22,3 +22,44 @@ $(document).ready(function(){
     } // End if
   });
 });
+$(function(){
+  
+  var
+      $outerSquare = $('.cube'),
+    $menus     = $('a.item'),
+    $innerSquare = $('.center'),
+    $menuParent = $menus.parent(),
+    $box       = $('.shape')
+    ;
+  
+  $outerSquare
+   .click('$flip', function() {
+    $box.shape('flip up');
+    })
+;
+$menus.detach();
+  $menus
+    .click( '$menus', function() {
+        $(this)
+         .addClass('active')
+          .siblings('.item')
+            .removeClass('active');
+  
+    })
+;
+$menuParent.append( $menus );
+  
+  $menus.on('click', function(attr) {
+    var tabId = $(this).attr('data-tab');
+    $box.shape(tabId);
+  })
+;
+  
+  $innerSquare
+       .mouseover('$flip', function() {
+    $box.shape('flip left');
+    })
+  ;
+  
+  
+});
